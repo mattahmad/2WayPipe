80929d67c402a00ce92edcecdda376defef41950
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-14 22:57:19 -0400

    Initial Commit

---
+++ 2WayPipe/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  2WayPipe
+//
+//  Created by Matt Ahmad on 2022-10-14.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
